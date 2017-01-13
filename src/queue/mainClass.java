f36167a2b70fad0150e760c10ee0bf666d0ed309
dhruv3 <dhruv.mnit@gmail.com>
2017-01-13 18:27:09 -0500

    1. LL based queue implemented. 2. Test case added for LL Queue

--- src/queue/mainClass.java
+++ src/queue/mainClass.java
@@ -16,11 +16,29 @@ public class mainClass {
 			testArr(fixedArrQ);
 			break;
 		case "2":
+			LinkedQueue llQ = new LinkedQueue();
+			testLL(llQ);
 			break;
 		default:
 			break;
 		}
 		
+	}
+
+	private static void testLL(LinkedQueue llQ) throws Exception {
+		System.out.println("Is queue empty? " + llQ.isEmpty());
+		llQ.enqueue(1);
+		llQ.enqueue(2);
+		llQ.enqueue(3);
+		llQ.enqueue(4);
+		llQ.enqueue(5);
+		System.out.println("Queue created is- " + llQ.toString());
+		System.out.println("First element is- " + llQ.first());
+		System.out.println("Queue size is- " + llQ.getSize());
+		System.out.println("On dequeuing 3 times we get- " + llQ.dequeue() + ", " + llQ.dequeue() + ", " + llQ.dequeue());
+		System.out.println("New queue created is- " + llQ.toString());
+		System.out.println("New first element is- " + llQ.first());
+		System.out.println("Queue size is- " + llQ.getSize());
 	}
 
 	private static void testArr(FixedSizeArrayQueue fixedArrQ) throws Exception {
