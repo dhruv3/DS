7a6e9d704beb7cb59c8005ff28634af95cd47e83
dhruv3 <dhruv.mnit@gmail.com>
2017-01-18 21:53:24 -0500

    MainClass with infinite looping Preorder traversal code

---
+++ src/BinaryTree/mainClass.java
@@ -0,0 +1,37 @@
+package BinaryTree;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		BinaryTreeNode root = createTree();
+		PreOrder(root);
+	}
+
+	//each node is processed BEFORE either of its subtree
+	private static void PreOrder(BinaryTreeNode root) {
+		while(root != null){
+			System.out.println(root.getData());
+			PreOrder(root.getLeft());
+			PreOrder(root.getRight());
+		}
+	}
+
+	private static BinaryTreeNode createTree() {
+		BinaryTreeNode n1 = new BinaryTreeNode(1);
+		BinaryTreeNode n2 = new BinaryTreeNode(2);
+		BinaryTreeNode n3 = new BinaryTreeNode(3);
+		n1.setLeft(n2);
+		n1.setRight(n3);
+		BinaryTreeNode n4 = new BinaryTreeNode(4);
+		BinaryTreeNode n5 = new BinaryTreeNode(5);
+		n2.setLeft(n4);
+		n2.setRight(n5);
+		BinaryTreeNode n6 = new BinaryTreeNode(6);
+		BinaryTreeNode n7 = new BinaryTreeNode(7);
+		n3.setLeft(n6);
+		n3.setRight(n7);
+		BinaryTreeNode root = n1;
+		return root;
+	}
+
+}
