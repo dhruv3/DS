67b0c2ebc0880eb76d1095049e16f0540659b2f6
dhruv3 <dhruv.mnit@gmail.com>
2017-02-06 14:41:34 -0500

    All botched code and everything

--- src/hashing/HashTableOperations.java
+++ src/hashing/HashTableOperations.java
@@ -5,12 +5,51 @@ public class HashTableOperations {
 	
 	public static HashTable createHashTable(int size){
 		HashTable hTable = new HashTable();
+		//LoadFactor = (# elements in hash table) / (hash table size)
+		//So, hash table size = (# elements in hash table) / (LoadFactor)
 		hTable.setSize(size/LOADFACTOR);
+
+		//for each entry in hash table set start node to null
 		for(int i = 0; i < hTable.getSize(); i++){
 			hTable.getTable()[i].setStartNode(null);
 		}
+		
 		return hTable;
 	}
 	
+	public static int Hash(int key, int size){
+		return (key % size);
+	}
+	
+	public static int hashSearch(HashTable h, int data){
+		ListNode temp;
+		
+		temp = h.getTable()[Hash(data, h.getSize())].getStartNode();
+		
+		while(temp != null){
+			if(temp.getData() == data)
+				return 1;
+			temp = temp.getNext();
+		}
+		
+		return 0;
+	}
+	
+	public static void hashInsert(HashTable h, int data){
+		int index;
+		ListNode temp, newNode;
+		
+		if(hashSearch(h, data) == 1)
+			return;
+		
+		index = Hash(data, h.getSize());
+		temp = h.getTable()[index].getStartNode();
+		
+		newNode = new ListNode();
+		newNode.setKey(index);
+		newNode.setData(data);
+		//newNode.setNext(h.getTable()[index].get);
+	}
 	
 }
+
