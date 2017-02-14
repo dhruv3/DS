f702fb70a945cc4a3080dbc1ec086a004ef8dd42
dhruv3 <dhruv.mnit@gmail.com>
2017-02-13 21:58:27 -0500

    hashmap g4g code

---
+++ src/hashMap/HashNode.java
@@ -0,0 +1,17 @@
+package hashMap;
+
+//A node of chains
+public class HashNode<K,V> {
+	K key;
+	V value;
+	
+	// Reference to next node
+	HashNode<K, V> next;
+	
+	// Constructor
+	public HashNode()
+	{
+		this.key=key;
+		this.value=value;
+	}
+}
