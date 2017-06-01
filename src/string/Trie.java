843dfad4232aa322e5e6a585f0faba55ff28b5c3
dhruv3 <dhruv.mnit@gmail.com>
2017-05-31 20:25:53 -0400

    Complete implementation with search and delete

--- src/string/Trie.java
+++ src/string/Trie.java
@@ -30,8 +30,55 @@ public class Trie {
 			node = node.children.get(inpArr[i]);
 		}
 		
-		node.endOfStirng = false;
+		node.endOfStirng = true;
 	}
 	
+	public boolean search(String input){
+		TrieNode node = root;
+		char[] inpArr = input.toCharArray();
+		
+		for(int i = 0; i < inpArr.length; i++){
+			if(!node.children.containsKey(inpArr[i])){
+				return false;
+			}
+			node = node.children.get(inpArr[i]);
+		}
+		
+		if(node.endOfStirng == true){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	 public void delete(String word) {
+	        delete(root, word, 0);
+	    }
 
+    private boolean delete(TrieNode current, String word, int index) {
+        if (index == word.length()) {
+            //when end of word is reached only delete if currrent.endOfWord is true.
+            if (!current.endOfStirng) {
+                return false;
+            }
+            current.endOfStirng = false;
+            //if current has no other mapping then return true
+            return current.children.size() == 0;
+        }
+        char ch = word.charAt(index);
+        TrieNode node = current.children.get(ch);
+        if (node == null) {
+            return false;
+        }
+        boolean shouldDeleteCurrentNode = delete(node, word, index + 1);
+
+        //if true is returned then delete the mapping of character and trienode reference from map.
+        if (shouldDeleteCurrentNode) {
+            current.children.remove(ch);
+            //return true if no mappings are left in the map.
+            return current.children.size() == 0;
+        }
+        return false;
+    }
 }
--- src/string/TrieMainClass.java
+++ src/string/TrieMainClass.java
@@ -1,10 +1,28 @@
 package string;
 
+//tutorial: https://www.youtube.com/watch?v=AXjmTQ8LEoI
 public class TrieMainClass {
 
 	public static void main(String[] args) {
 		Trie myTrie = new Trie();
 		testInsert(myTrie);
+		System.out.println("Checking search implementation-----------------------------");
+		testSearch(myTrie);
+		System.out.println("Checking delete implementation-----------------------------");
+		testDelete(myTrie);
+	}
+
+	private static void testDelete(Trie myTrie) {
+		System.out.println("Is \'abc\' is present: " + myTrie.search("abc"));
+		System.out.println("Deleting 'abc'------------");
+		myTrie.delete("abc");
+		System.out.println("Is \'abc\' is present: " + myTrie.search("abc"));
+	}
+
+	private static void testSearch(Trie myTrie) {
+		System.out.println("Is \'abc\' is present: " + myTrie.search("abc"));
+		System.out.println("Is \'abcd\' is present: " + myTrie.search("abcd"));
+		System.out.println("Is \'abqc\' is present: " + myTrie.search("abqc"));
 	}
 
 	private static void testInsert(Trie myTrie) {
