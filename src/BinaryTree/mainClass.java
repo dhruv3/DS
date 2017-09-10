ab7ad15330c5d45387142dc7da2ecd967b29d312
Dhruv Dogra <dhruv.mnit@gmail.com>
2017-09-10 11:26:24 -0400

    TODO: Add attribute in data structure to store parent node

--- src/BinaryTree/mainClass.java
+++ src/BinaryTree/mainClass.java
@@ -47,16 +47,18 @@ public class mainClass {
 		BinaryTreeNode n2 = new BinaryTreeNode(2);
 		BinaryTreeNode n3 = new BinaryTreeNode(3);
 		n1.setLeft(n2);
-		n1.setRight(n3);
+		//n1.setRight(n3);
 		BinaryTreeNode n4 = new BinaryTreeNode(4);
 		BinaryTreeNode n5 = new BinaryTreeNode(5);
 		n2.setLeft(n4);
 		n2.setRight(n5);
-		BinaryTreeNode n6 = new BinaryTreeNode(6);
+		n5.setLeft(n3);
+		BinaryTreeNode n8 = new BinaryTreeNode(8);
 		BinaryTreeNode n7 = new BinaryTreeNode(7);
-		n3.setLeft(n6);
-		n3.setRight(n7);
-		
+		n7.setLeft(n8);
+		n1.setRight(n7);
+		BinaryTreeNode n9 = new BinaryTreeNode(9);
+		n8.setRight(n9);
 		BinaryTreeNode root = n1;
 		
 		return root;
